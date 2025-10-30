8fc4ef7a0a073148ba3f533a1f31eddbfa5e5e24
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-30 21:36:39 +0300

    fix

--- update_repository.sh
+++ update_repository.sh
@@ -35,7 +35,7 @@ merge_branches()
         git push
         complete_branches+=("$branch")
       else
-        if [$PARAM == 's']; then
+        if ["$PARAM" = "s"]; then
           git merge --abort
           skipped_branches+=("$branch")
         else 
