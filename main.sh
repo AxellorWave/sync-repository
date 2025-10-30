4d12c0310c7a5bde17e2f7979eb01888355112c4
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-30 19:46:08 +0300

    added params of merging

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]]; then
+    PARAM="--help"
+else
+    PARAM="$1"
+fi
+
 if [ ! -f "config.conf" ]; then
   echo "Ошибка: Отсутствует файл config.conf"
   exit 1
@@ -31,23 +37,65 @@ check_upstream()
 
 merge_branches()
 {
+  skipped_branches=()
+  complete_branches=()
   for branch in $(git branch --format='%(refname:short)'); do
     if [ "$branch" != "master" ]; then
       git switch $branch
-      git merge master
-      git push
+      if git merge master; then
+        git push
+        complete_branches+=("$branch")
+      else
+        if [$PARAM == 's']; then
+          git merge --abort
+          skipped_branches+=("$branch")
+        else 
+          echo "Конфликт. Ветка: $branch"
+          echo "Обновленные ветки:"
+          for for branch in "${complete_branches[@]}"; do
+            echo "* $branch"
+          done
+          return 0
+        fi  
+      fi
     fi
   done
+  echo "Синхронизация окончена"
+  echo "Обновленные ветки:"
+  for for branch in "${complete_branches[@]}"; do
+    echo "* $branch"
+  done
+  echo "Пропущенные ветки:"
+  for for branch in "${skipped_branches[@]}"; do
+    echo "* $branch"
+  done
+  return 0
 }
 
 main()
 {
+  case "$PARAM" in
+    --help|-h)
+        echo "Help text"
+        exit 0
+        ;;
+    --skip-conflict|-s)
+        $PARAM = "s"
+        ;;
+    --stop-conflict|-c)
+        $PARAM = "c"
+        ;;
+    *)
+        echo "Неизвестный параметр: $PARAM" >&2
+        exit 1
+        ;;
+esac
   if check_upstream; then
+    git switch master
+    git fetch upstream
+    git merge upstream/master
+    git push
     merge_branches
-    #git switch master
-    #git fetch upstream
-    #git merge upstream/master
-    #git push
   else 
     exit 1
   fi
