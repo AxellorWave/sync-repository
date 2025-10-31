e11ecde6dcb2ce5a40e271f250a1987ad67cc062
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-31 21:24:19 +0300

    Formatted according to standards and a logical error has been corrected.

--- sync-rep.sh
+++ sync-rep.sh
@@ -1,75 +1,83 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]]; then
-    PARAM="--help"
+  PARAM="--help"
 else
-    PARAM="$1"
+  PARAM="$1"
 fi
 
 is_script_ignored() {
-    local script_name=$(basename "$0")
-    local global_excludes=$(git config --global core.excludesFile)
-    if [ -n "$global_excludes" ] && [ -f "$global_excludes" ] && grep -q "^$script_name$" "$global_excludes"; then
-        return 0
-    else
-        return 1
-    fi
+  local script_name
+  script_name=$(basename "$0")
+  local global_excludes
+  global_excludes=$(git config --global core.excludesFile)
+
+  if [[ -n "$global_excludes" ]] && [[ -f "$global_excludes" ]] && grep -q "^${script_name}$" "$global_excludes"; then
+    return 0
+  else
+    return 1
+  fi
 }
 
-check_upstream() 
-{
+check_upstream() {
   if ! git remote get-url upstream &>/dev/null; then
     echo "Upstream не настроен"
     echo "Для настройки введите команду:"
-    echo "git remote add upstream <repository_link>" 
+    echo "git remote add upstream <repository_link>"
     return 1
   fi
-  if [ "$(git remote get-url upstream)" = "$(git remote get-url origin)" ]; then
+
+  if [[ "$(git remote get-url upstream)" == "$(git remote get-url origin)" ]]; then
     echo "origin и upstream совпадают"
-    echo "Изменити ссылку на upstream с помощью команд:"
+    echo "Измените ссылку на upstream с помощью команд:"
     echo "git remote remove upstream"
-    echo "git remote add upstream <repository_link>" 
+    echo "git remote add upstream <repository_link>"
     return 1
   fi
+
   return 0
 }
 
-merge_branches()
-{
-  skipped_branches=()
-  complete_branches=()
+merge_branches() {
+  local skipped_branches=()
+  local complete_branches=()
+  local branch
+
   for branch in $(git branch --format='%(refname:short)'); do
-    if [ "$branch" != "master" ]; then
-      git switch $branch
-      if git ls-remote --heads origin "$branch" && git merge master; then
+    if [[ "$branch" != "master" ]]; then
+      git switch "$branch"
+
+      if git ls-remote --heads origin "$branch" | grep -q . && git merge master; then
         git push
         complete_branches+=("$branch")
       else
         if [[ "$PARAM" == "s" ]]; then
           git merge --abort
           skipped_branches+=("$branch")
-        else 
-          if ! git ls-remote --heads origin "$branch"; then
+        else
+          if ! git ls-remote --heads origin "$branch" | grep -q .; then
             git merge master
-          fi  
-          for branch in "${complete_branches[@]}"; do
-            echo "* $branch"
+          else 
+            git push
+          fi
+          for complete_branch in "${complete_branches[@]}"; do
+            echo "* $complete_branch"
           done
           return 0
-        fi  
+        fi
       fi
     fi
   done
-  for branch in "${skipped_branches[@]}"; do
-    echo "* $branch"
+
+  for skipped_branch in "${skipped_branches[@]}"; do
+    echo "* $skipped_branch"
   done
-  git switch master
+
+  git switch master &>/dev/null
   return 0
 }
 
-main()
-{
-
+main() {
   case "$PARAM" in
     --help|-h)
       echo "ОПИСАНИЕ"
@@ -92,10 +100,10 @@ main()
       echo "  • Настройка игнорирования скрипта:"
       echo "    Если файла с игнором не было раньше"
       echo "    (проверить через git config --global core.excludesFile)"
-      echo "       mkdir -p ~/.config/git/ && echo "$(basename "$0")" >> ~/.config/git/ignore"
+      echo "       mkdir -p ~/.config/git/ && echo \"$(basename "$0")\" >> ~/.config/git/ignore"
       echo "       git config --global core.excludesFile ~/.config/git/ignore"
       echo "    Если файл был"
-      echo '       echo '$(basename "$0")' >> $(git config --global core.excludesFile)'
+      echo "       echo \"$(basename "$0")\" >> \"\$(git config --global core.excludesFile)\""
       echo ""
       echo "ПРОЦЕСС РАБОТЫ"
       echo "  1. Переключение на master ветку"
@@ -110,7 +118,7 @@ main()
       echo "  ./$(basename "$0") -h               # Вывод справки"
       echo ""
       exit 0
-    ;;
+      ;;
     --skip-conflict|-s)
       PARAM="s"
       ;;
@@ -121,25 +129,27 @@ main()
       echo "Неизвестный параметр: $PARAM" >&2
       exit 1
       ;;
-esac
+  esac
+
   if ! is_script_ignored; then
     echo "WARNING! Скрипт не добавлен в ignore"
     echo "Используйте следующие команды:"
     if ! git config --global core.excludesFile &>/dev/null; then
-      echo 'mkdir -p ~/.config/git/ && echo '$(basename "$0")' >> ~/.config/git/ignore'
+      echo "mkdir -p ~/.config/git/ && echo \"$(basename "$0")\" >> ~/.config/git/ignore"
       echo "git config --global core.excludesFile ~/.config/git/ignore"
     else
-      echo 'echo "$(basename "$0")" >> $(git config --global core.excludesFile)'
+      echo "echo \"$(basename "$0")\" >> \"\$(git config --global core.excludesFile)\""
     fi
     echo ""
   fi
+
   if check_upstream; then
     git switch master
     git fetch upstream
     git merge upstream/master
     git push
     merge_branches
-  else 
+  else
     exit 1
   fi
 }
